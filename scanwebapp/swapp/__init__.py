c2aa409b0bc524065d2f98f409179aa36f5ca86b
Tyler Bartle <tman.bartleboy1@gmail.com>
2025-05-09 00:06:40 -0600

    Basic basic basic framework, setup, confirmed working password auth & hashing

---
+++ scanwebapp/swapp/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    load_dotenv()
+
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "fallback-secret")
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    from swapp.models import User
+    from swapp.auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    @app.route('/')
+    def dashboard():
+        from flask_login import login_required, current_user
+        return login_required(lambda: render_template('dashboard.html', name=current_user.username))()
+
+    return app
---
+++ scanwebapp/swapp/auth.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required
+from swapp.models import User
+from swapp import db
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and check_password_hash(user.password, request.form['password']):
+            login_user(user)
+            return redirect(url_for('dashboard'))
+        flash('Invalid credentials')
+    return render_template('login.html')
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        hashed_pw = generate_password_hash(request.form['password'])
+        new_user = User(username=request.form['username'], password=hashed_pw)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('User created. You can now log in.')
+        return redirect(url_for('auth.login'))
+    return render_template('register.html')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
